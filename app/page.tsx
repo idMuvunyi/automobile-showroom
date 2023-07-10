64fa316b6c5ee0b23de4530ec9d885a6b598f47b
Muvunyi Idrissa <muvunyiiddy@gmail.com>
2023-07-10 11:39:07 +0200

    design custom search ui inputs

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,23 @@
-import { LandingPage } from '@/components';
+import { CustomFilter, LandingPage, SearchBar } from '@/components';
 import Image from 'next/image';
 
 export default function Home() {
   return (
     <main className="overflow-hidden">
       <LandingPage />
+      <div className="mt-12 padding-x padding-y max-width" id="explore">
+        <div className="home__text-container">
+          <h1 className="text-4xl font-extrabold">Car Listing</h1>
+          <p>Explore the vehicles you might be interested in!</p>
+        </div>
+        <div className="home__filters">
+          <SearchBar />
+          <div className="home__filter-container">
+            <CustomFilter title="fuel" />
+            <CustomFilter title="year" />
+          </div>
+        </div>
+      </div>
     </main>
   );
 }
